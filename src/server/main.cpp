080ce71c13ab8464352e27e21f2f6d4238bc8797
winzihao <winzihao@163.com>
2021-02-17 19:05:26 +0800

    提交项目所有代码

---
+++ src/server/main.cpp
@@ -0,0 +1,41 @@
+//使用了muduo库，得到了一个非常强大的，基于事件驱动的，IO复用epoll+线程池的网络代码
+//完全是基于reactor模型的，有一个主reactor,是IO线程，三个server reactor,是工作线程
+//主reactor负责新用户的链接，子reactor负责已连接用户读写事件的处理
+
+#include "chatserver.hpp"
+#include "chatservice.hpp"
+#include <iostream>
+#include <signal.h>
+using namespace std;
+
+// 处理服务器ctrl+c结束后，重置user的状态信息
+void resetHandler(int)
+{
+    ChatService::instance()->reset();
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        cerr << "command invalid! example: ./ChatServer 127.0.0.1 6000" << endl;
+        exit(-1);
+    }
+
+    // 解析通过命令行参数传递的ip和port
+    char *ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    signal(SIGINT, resetHandler);
+
+    EventLoop loop;
+    InetAddress addr(ip, port);
+    ChatServer server(&loop, addr, "ChatServer");
+
+    server.start();
+    loop.loop();
+
+    return 0;
+}
+
